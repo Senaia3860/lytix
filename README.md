371ee7fb2d74c243d4379a64417d9a28a03ef223
Senaia3860 <bedwan@hotmail.com>
2019-11-30 11:41:34 +0200

    Initial Commit.

---
+++ README.md
@@ -0,0 +1,42 @@
+Lytix Core repository
+=====================================
+
+[![Build Status](https://travis-ci.org/LytixChain/lytix.png)](https://travis-ci.org/LytixChain/lytix) [![GitHub tag](https://img.shields.io/github/tag/LytixChain/lytix.svg)](https://github.com/lytixchain/lytix/tree/v1.1.4)
+
+Lytix is an open source crypto-currency focused on fast private transactions with low transaction fees & low confirmations.  It is currently in production. We are in our Proof of Stake phase.  The base Maxnode system is in place and we are currently working on the data insertion utility.
+
+------------------------
+
+Proof of Stake is the current Lytix mechanism for obtaining coins outside of buying directly on an exchange
+
+10,000 LYTX for Masternodes
+
+100,000 LYTX for Maxnodes
+
+Reward breakdown -  60% Maxnodes, 20% masternodes and 20% Stakers.
+
+30 LYTX reward until 100,000
+- 100 until 110,000
+- 30 until 300,000
+- 15 until 350,000
+- 10 until 700,000
+- 7 until 1,050,000
+- 5 until 1,400,000
+- 3 until 1,750,000
+- 1 from then on 
+
+16 confirmations maturity for mined or staked rewards
+ZeroCoin has been removed from the wallet and is in maitnenance mode on Mainnet
+
+-------------------------
+
+Other Information
+
+- Website - http://www.lytixchain.org
+- Block Explorer - http://explorer.lytixchain.org
+- Twitter - https://twitter.com/ChainLytix
+- Telegram - https://t.me/lytixcoin
+- Discord - https://discord.gg/B6QXCPq
+- YouTube - https://www.youtube.com/channel/UCe9mVDCKp_xtH7tvN2Z17iA
+- Medium - https://medium.com/@faetoslytix
+- Exchange - https://crex24.com/exchange/LYTX-BTC
---
+++ src/qt/mnaddresspage.cpp
@@ -0,0 +1,72 @@
+// Copyright (c) 2018 The Phore developers
+// Copyright (c) 2018 The Curium developers
+// Copyright (c) 2018-2019 The Lytix developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+
+#include "privkeypage.h"
+#include "ui_privkeypage.h"
+
+#include "bitcoingui.h"
+#include "csvmodelwriter.h"
+#include "editaddressdialog.h"
+#include "guiutil.h"
+#include "master/node-budget.h"
+#include "master/node-payments.h"
+#include "master/nodeconfig.h"
+#include "master/nodeman.h"
+
+#include <univalue.h>
+#include <QIcon>
+#include <QMenu>
+#include <QString>
+#include <QMessageBox>
+#include <QSortFilterProxyModel>
+
+MNAddressPage::MNAddressPage(QWidget* parent) : QDialog(parent),
+                                            ui(new Ui::MNAddressPage)
+{
+    ui->setupUi(this);
+
+#ifdef Q_OS_MAC // Icons on push buttons are very uncommon on Mac
+    ui->copyAddress->setIcon(QIcon());
+#endif
+	key = MNAddressPage::createmasternodekey();
+	ui->payTo->setText(key);
+
+    // Context menu actions
+    QAction* copyAddressAction = new QAction(tr("&Copy Address"), this);
+
+    // Build context menu
+    contextMenu = new QMenu();
+    contextMenu->addAction(copyAddressAction);
+    contextMenu->addSeparator();
+
+    // Connect signals for context menu actions
+    connect(copyAddressAction, SIGNAL(triggered()), this, SLOT(on_copyAddress_clicked()));
+
+    connect(ui->closeButton, SIGNAL(clicked()), this, SLOT(accept()));
+}
+
+
+
+MNAddressPage::~MNAddressPage()
+{
+    delete ui;
+}
+
+
+void MNAddressPage::on_copyAddress_clicked()
+{
+     GUIUtil::setClipboard(ui->payTo->text());
+
+}
+
+QString MNAddressPage::createmasternodekey()
+{
+    CKey secret;
+    secret.MakeNewKey(false);
+
+    return QString::fromStdString(CBitcoinSecret(secret).ToString());
+}
